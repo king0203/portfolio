8e74103ff25a1066263ca27758f0254ddc16e701
Darshan <darshanarkhade@gmail.com>
2023-06-29 03:33:13 +0530

    particles

---
+++ components/ParticlesContainer.js
@@ -0,0 +1,95 @@
+"use-client";
+import React from "react";
+import { Particles } from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+
+const ParticlesContainer = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+  const particlesLoaded = useCallback(async () => {}, []);
+
+  return (
+    <Particles
+      className="w-full h-min-screen absolute -translate-z-2"
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: { enable: false },
+        background: {
+          color: {
+            value: "",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 70,
+            },
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#000000",
+          },
+          links: {
+            distance: 100,
+            enable: true,
+            obacity: 0,
+            width: 0,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 900,
+            },
+            value: 200,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+export default ParticlesContainer;
